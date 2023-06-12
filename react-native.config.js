b62efdc06ce5b0e541886f47cfa4cfc9880b24a7
Philip Cesar Garay <philipgaray2@gmail.com>
2023-06-12 21:10:49 +0800

    Update for iOS

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dependencies: {
+    ...(process.env.NO_FLIPPER
+      ? {'react-native-flipper': {platforms: {ios: null}}}
+      : {}),
+  },
+  project: {
+    ios: {},
+    android: {},
+  },
+};
