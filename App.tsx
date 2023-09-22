d06fdcec53ea4f8e4bda93963b526029c5867147
Philip Cesar Garay <philipgaray2@gmail.com>
2023-09-22 16:33:37 +0800

    Update

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import RootNavigators from './src/navigations';
 import { persistor, store } from './src/stores';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 declare var console: any;
 declare var global: any;
@@ -29,14 +29,16 @@ const App = () => {
   }
 
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <SafeAreaProvider>
-          <StatusBar barStyle="default" />
-          <RootNavigators />
-        </SafeAreaProvider>
-      </PersistGate>
-    </Provider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <SafeAreaProvider>
+            <StatusBar barStyle="default" />
+            <RootNavigators />
+          </SafeAreaProvider>
+        </PersistGate>
+      </Provider>
+    </GestureHandlerRootView>
   );
 };
 
